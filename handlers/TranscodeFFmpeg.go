330999e3567ee90ff2a73a7f5837d00f11219dee
carlinhoshk <carlosmdohk@gmail.com>
2023-01-20 00:23:04 -0300

    16

--- handlers/TranscodeFFmpeg.go
+++ handlers/TranscodeFFmpeg.go
@@ -1,11 +1,14 @@
 package handlers
 
 import (
-    "fmt"
+    "github.com/Azure/azure-storage-blob-go/azblob"
     "log"
-    "os"
     "os/exec"
+    "context"
     "net/http"
+    "fmt"
+    "os"
+    "net/url"
 )
 
 func TranscodeFFmpeg(w http.ResponseWriter, r *http.Request) {
@@ -13,6 +16,29 @@ func TranscodeFFmpeg(w http.ResponseWriter, r *http.Request) {
         w.WriteHeader(http.StatusMethodNotAllowed)
         return
     }
+
+    // Define as credenciais de armazenamento
+    accountName := "bloobstream"
+    accountKey := os.Getenv("TOKEN_AZURE_BLOB")
+
+    // Define o nome do contêiner e do arquivo de vídeo
+    containerName := "cinema-storage"
+    videoFileName := "teste.mp4"
+
+    // Cria um ponto de extremidade de blob
+    credential, err := azblob.NewSharedKeyCredential(accountName, accountKey)
+    if err != nil {
+        fmt.Println("Erro ao criar as credenciais:", err)
+        return
+    }
+
+    p := azblob.NewPipeline(credential, azblob.PipelineOptions{})
+    url, _ := url.Parse(
+        fmt.Sprintf("https://%s.blob.core.windows.net/%s", accountName, containerName))
+
+    // Abre o contêiner
+    containerURL := azblob.NewContainerURL(*url, p)
+    blobURL := containerURL.NewBlobURL(videoFileName)
     // opens the file from the request
     file, header, err := r.FormFile("file")
     if err != nil {
@@ -21,12 +47,30 @@ func TranscodeFFmpeg(w http.ResponseWriter, r *http.Request) {
     }
     defer file.Close()
 
-    // creates the transcoded file
-    transcodedFile, err := os.Create(header.Filename + ".transcoded")
+    // opens a new file to write the transcoded file
+    transcodedFile, err := os.Create(header.Filename)
     if err != nil {
         fmt.Println("Error creating transcoded file:", err)
         return
     }
     defer transcodedFile.Close()
 
-    // runs the ffmpeg command to 
+    // runs the ffmpeg command to transcode the video
+    cmd := exec.Command("ffmpeg", "-i", header.Filename, "-c:v", "libx264", "-b:v", "3000k", transcodedFile.Name())
+    err = cmd.Run()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // uploads the transcoded file to the container
+    blockBlobURL := blobURL.ToBlockBlobURL()
+    _, err = azblob.UploadFileToBlockBlob(context.Background(), transcodedFile, blockBlobURL, azblob.UploadToBlockBlobOptions{
+        BlockSize:   4 * 1024 * 1024,
+        Parallelism: 16})
+    if err != nil {
+        fmt.Println("Error uploading file to blob storage:", err)
+        return
+    }
+    fmt.Fprintf(w, "File %s transcoded and uploaded successfully", header.Filename)
+}
+
--- main.go
+++ main.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/carlinhoshk/stream-go/handlers/transcode"
+	"github.com/carlinhoshk/stream-go/handlers"
 )
 
 func main() {
-	http.HandleFunc("/transcode", transcode)
+	http.HandleFunc("/transcode", handlers.TranscodeFFmpeg)
 	// Inicia o servidor na porta 9090
 	fmt.Println("Servidor iniciado na porta 9090")
 	http.ListenAndServe(":9090", nil)
